43f6b3ea1b868b86f934822cfdea026c761fba75
Ferkalek <ferkalekanton@gmail.com>
2020-04-01 09:28:48 +0200

    add html pages

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+const rootDir = require('./utils/path');
 
 const app = express();
 
-app.use('/list', (req, res, next) => {
-    res.send('<h1>List</h1>');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public'))); // add public path
 
-app.use('/', (req, res, next) => {
-    res.send('<h1>Simple version</h1>');
+app.use(adminRouter);
+app.use(shopRouter);
+
+// add error 404
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3002);
